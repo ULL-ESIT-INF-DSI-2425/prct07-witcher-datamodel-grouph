f5552c09559a4cc190d604ab0eec2fe6bd823cf7
Zake207 <airampg207@gmail.com>
2025-03-14 19:49:19 +0000

    feat: Added  getters y setters

--- src/merchant.ts
+++ src/merchant.ts
@@ -8,11 +8,37 @@ export type Profession =
 
 export class Merchant {
   constructor(
-    public readonly id: number,
-    public readonly name: string,
-    public readonly profession: Profession,
-    public readonly location: string,
+    private _id: number,
+    private _name: string,
+    private _profession: Profession,
+    private _location: string,
   ) {
-    this.id = id + 20000;
+    this._id = _id + 20000;
+  }
+
+  get id() {
+    return this._id
+  }
+  get name() {
+    return this._name
+  }
+  get profession() {
+    return this._profession
+  }
+  get location() {
+    return this._location
+  }
+
+  set id(new_id: number) {
+    this._id = new_id
+  }
+  set name(new_name: string) {
+    this._name = new_name
+  }
+  set profession(new_profession: Profession) {
+    this._profession = new_profession
+  }
+  set location(new_location: string) {
+    this._location = new_location
   }
 }
