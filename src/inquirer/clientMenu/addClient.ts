20fddf920cbe77ccf380290cd0a8773f896dbd93
ValBoschP <alu0101485287@ull.edu.es>
2025-03-22 13:20:53 +0000

    feat: add validation for `add client` race input

--- src/inquirer/clientMenu/addClient.ts
+++ src/inquirer/clientMenu/addClient.ts
@@ -1,16 +1,36 @@
 import inquirer from "inquirer";
 import { displayTitle, pressEnterToContinue } from "../utils/menuUtils.js";
 import { clientDB, clientMenu } from "./clientMenu.js";
-import { Hunter } from "../../hunter.js";
+import { Hunter, Race } from "../../hunter.js";
+
+const validRaces: Race[] = [
+  "Human",
+  "Elf",
+  "Dwarf",
+  "Halfling",
+  "Warlock",
+  "Lycanthropic",
+  "Vran",
+  "Dryad",
+  "Spectral Cat",
+  "Half-Elf",
+] as const;
 
 export function addClient(): void {
   displayTitle("Add Client");
   inquirer
     .prompt([
       { type: "input", name: "name", message: "Enter the client's name:" },
-      { type: "input", name: "race", message: "Enter the client's race:" },
-      { type: "input", name: "address", message: "Enter the client's address:",
+      { type: "input", name: "race", message: "Enter the client's race:",
+        validate: (input) => {
+          const trimmedInput = input.trim() as Race;
+          return validRaces.includes(trimmedInput)
+            ? true
+            : `Invalid race. Choose form ${validRaces.join(", ")}.`;
+        }
       },
+
+      { type: "input", name: "address", message: "Enter the client's address:" },
     ])
     .then((answers) => {
       const newId = Date.now().toString();
